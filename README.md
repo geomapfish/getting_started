26ca75d6dd47a4d97d55a691d2a50137627cd091
Guillaume <guillaume.remy@bs.ch>
2021-03-04 15:32:03 +0100

    Getting Started - first version

--- README.md
+++ README.md
@@ -1,2 +1,218 @@
-# getting_started
-Getting Started with GeoMapFish
+# Getting Started with GeoMapFish - **TL; DR**
+
+Open a linux console, and execute the following commands:
+```
+wget https://raw.githubusercontent.com/geomapfish/getting_started/main/install.sh
+chmod +x install.sh
+./install.sh
+```
+
+Then, follow the instructions.  
+
+Your application is at https://localhost:8484.  
+The admin interface is at https://localhost:8484/admin.  
+The default credentials are admin/admin.
+
+That's it!  
+Enjoy :-)
+
+___
+# Getting Started with GeoMapFish - **Long version**
+
+## Introduction
+
+This page goes through the basic, required steps to create your first GeoMapFish application from scratch. It is based on **Version 2.5**. To learn what GeoMapFish is all about, please visit our [homepage](https://geomapfish.org/).
+
+This guide is written for Linux systems. GeoMpaFish works on Windows systems as well, but as the community is using Linux in a large majority, there is no guide for Windows at the moment.
+
+___
+## **Step 1: Configure proxies**
+
+If you are behind a corporate proxy, you will have to configure the proxies. This can be done like this:
+
+```
+export http_proxy=http://user:password@proxy_host:proxy_port
+export https_proxy=http://user:password@proxy_host:proxy_port
+export no_proxy=localhost,127.0.0.1,mydomain.ch
+```
+
+___
+## **Step 2: Install requirements**
+
+The first step is to install the GeoMapFish requirements.  
+You will need to have the following components installed on your system:
+
+- Git
+- Python >= 3.5
+- Docker >= 17.05
+- Docker-Compose
+
+You can install the requirements by using your package manager.  
+For example, on Debian, you can do the following:
+
+```
+apt update && apt install -y git python3 docker docker-compose
+```
+
+In order to be able to use GeoMapFish, your linux user mut be part of the docker group:
+
+```
+usermod -aG docker myuser
+```
+
+___
+## **Step 3: Prepare database**
+
+To store its configuration, GeoMapFish is using a PostgreSQL database with the following required extensions:
+
+- PostgreSQL >= 9.1
+- PostGIS >= 2.1
+- hstore
+- pg_trgm
+
+**If you don't have any Postgres database yet, the installation script can download and configure one for you. If this is what you want, you can jump directly to Step 4**.
+
+Otherwise, if you want to use you own database, the installation script will help you to configure GeoMapFish in order to use it, but you will have to execute first some commands on the database manually.
+
+### 1. Create database
+```
+CREATE DATABASE mydb;
+```
+
+### 2. Activate extensions:
+```
+CREATE EXTENSION postgis;
+CREATE EXTENSION hstore;
+CREATE EXTENSION pg_trgm;
+```
+
+### 3. Create User, Schema, and configure rights:
+```
+CREATE USER www PASSWORD 'secret';
+CREATE SCHEMA main;
+CREATE SCHEMA main_static;
+GRANT ALL ON SCHEMA main TO www;
+GRANT ALL ON SCHEMA main_static TO www;
+```
+
+___
+## **Step 4: Install GeoMapFish**
+
+You are now ready to install GeoMapFish.  
+Let's do it!
+
+### 1. Download the installation script
+```
+wget https://raw.githubusercontent.com/geomapfish/getting_started/main/install.sh
+```
+
+### 2. Make it executable
+```
+chmod +x install.sh
+```
+
+### 3. Execute it
+The installation script will guide you through the configuration process.  
+Just start the script, and follow the instructions:
+```
+./install.sh
+```
+
+The questions asked by the installation script should be self-explanatory.  
+If there's something you don't understand, feel free to ask for help at contact@geomapfish.org.  
+If you're encountering an error during the installation, please join the generated file `install.log` to your email.
+
+___
+## **Step 5: First start**
+
+You should now have a working instance of GeoMapFish.  
+Congratulations!
+
+Now, it's time to start your application for the first time. You should be able to do this at [https://hostname:port]() (for exemple https://localhost:8484).
+
+Because your new instance of GeoMapFish doesn't have any valid SSL certificat yet, you will very likely get a `NET::ERR_CERT_AUTHORITY_INVALID` warning from your Browser:
+
+![SSL Error](img/ssl_error.png)
+
+For the moment, just ignore it and proceed to website anyway.  
+You will be able to see the application:
+
+![First start](img/first_start.png)
+
+___
+## **Step 6: Login & change admin password**
+
+On the right side of the application, you will see a toolbar.  
+At the very top of this toolbar, you can login to your application.
+
+The default credentials are:
+- Username : **admin**
+- Password : **admin**
+
+![Admin Login](img/admin_login.png)
+
+Once logged in, you will have to change the admin password:
+
+![Change Password](img/change_pass.png)
+
+Ok, there you are !
+
+![Admin connected](img/admin_connected.png)
+
+Well... it's quite empty for now.  
+Let's add some data!
+
+___
+## **Step 7: Add basemap**
+
+If you want a nice mapping application, I guess you need a background map to get started.  
+Let's add a WMTS Layer from Swisstopo in order to use it as Basemap.
+
+### 1. Connect to the admin interface
+It is available at [https://hostname:port/admin]() (for exemple https://localhost:8484/admin).  
+As you are already logged in to GeoMapFish with the **admin** user, you are also automatically authenticated to the administration interface.
+
+### 2. Add a new WMTS Layer
+Navigate to the `WMTS Layers` tab:
+
+![WMTS Empty](img/wmts_empty.png)
+
+Create a new WMTS Layer, and configure it as follows:
+
+- **Name**: The name of the layer in GeoMapFish: `Swisstopo Pixelkarte Color`
+- **GetCapabilities URL**: The URL that will be used to get the Capabilities of the configured WMTS Layer: 
+  https://wmts.geo.admin.ch/1.0.0/WMTSCapabilities.xml. 
+- **WMTS layer name**: The identifier if the WMTS layer in the Capabilities: `ch.swisstopo.pixelkarte-farbe`. 
+- **Public**: Defines if this layer will be publicly available in the geoportal, or if it will have restricted access: `checked`
+- **Interfaces**: Defines in which interface this Layer will be available: `desktop` and `mobile`
+
+![WMTS Color](img/wmts_color.png)
+
+Click `Submit`.
+
+### 3. Add the Layer to background layers
+Navigate to the `Layer groups` tab:
+
+![Layer groups](img/groups.png)
+
+A layer group, with the name `background`, is already configured. This group is used to load available background maps in the application. The layers linked to this group will be visible in the application under the `basemap selector`:
+
+![Basemap Selector](img/basemap_selector.png)
+
+Let's add our new WMTS Layer to this group.  
+Open the layer group by double-clicking on it. In the `Children` area, select the WMTS Layer `Swisstopo Pixelkarte Color` and to add it the the group:
+
+![Add Basemap](img/add_basemap.png)
+
+Click `Submit`.
+
+### 4. Refresh the application
+
+Go back to your geoportal (https://localhost:8484), and refresh the website with `F5`. The new basemap should now be available in the background selector:
+
+![With Basemap](img/with_basemap.png)
+
+Congratulations, you've configured your first basemap in you geoportal!
+
+___
+## **Step 8: Add you own data**
---
+++ install.sh
@@ -0,0 +1,369 @@
+#!/bin/bash
+
+Default=$'\e[0m'
+Green=$'\e[1;32m'
+Blue=$'\e[1;34m'
+Red=$'\e[1;31m'
+
+declare -i gmf_port
+gmf_port=8484
+gmf_host=`hostname`
+using_proxy=false
+
+abort()
+{
+  echo "${Red}Aborting GeoMapFish installation...${Default}"
+  exit $1
+}
+
+# Requirements
+##############
+
+check()
+{
+  if command -v $1 > /dev/null
+  then
+    version=`$1 --version | head -1 2>&1`
+    echo "${Green}[OK]  $version"
+  else
+    echo "${Red}[NOK] $1 NOT FOUND"
+    echo "Please install the missing requirement."
+    abort 91
+  fi
+}
+
+checkuser()
+{
+  user=`whoami`
+  if getent group docker | grep -q $user
+  then
+    echo "${Green}[OK]  User $user is in group docker"
+  else
+    echo "${Red}[NOK] User $user is not in group docker"
+    abort 92
+  fi
+}
+
+checkport()
+{
+  for i in {8484..8500}
+  do
+    used=`ss -tunlp | grep 'LISTEN' | grep $i | wc -l`
+    if [ $used == 0 ]
+    then
+      gmf_port=$i
+      echo "${Green}[OK]  Port $gmf_port will be used by GeoMapFish"
+      return
+    fi
+  done
+  echo "${Red}[NOK] Cannot find any free port between 8484 and 8500 to start GMF."  
+  abort 93
+}
+
+echo
+echo "${Default}---------------------------------------------------------------------------"
+echo "${Default}Analysing requirements..."
+check 'git'
+check 'docker'
+check 'docker-compose'
+check 'python3'
+check 'ss'
+check 'sed'
+check 'wget'
+checkuser
+checkport
+
+# Proxy configuration
+#####################
+
+proxy()
+{
+  if ! [ -z ${!1} ]
+  then 
+    echo "${Green}$1: set to ${!1}"
+    using_proxy=true
+    return
+  fi
+
+  up=${1^^}
+  if [ -z ${!up} ]
+  then 
+    echo "${Green}${1^^}: <not set>"
+  else
+    echo "${Green}${1^^}: set to ${!up}"
+    using_proxy=true
+  fi
+}
+
+echo
+echo "${Default}--------------------------------------------------------------------------"
+echo "${Default}If you are behind a proxy, the environment variables should be configured."
+echo "Please verify that the following configuration is correct:"
+proxy 'http_proxy'
+proxy 'https_proxy'
+proxy 'no_proxy'
+
+read -p "${Default}Do you want to continue with this configuration? [y/n] " -n 1 -r cont
+echo
+if ! [[ $cont =~ ^[Yy]$ ]]
+then
+  abort 96
+fi
+ 
+# GeoMapFish configuration
+##########################
+
+echo
+echo "${Default}--------------------------------------------------------------------------"
+echo "Ok, let's configure GeoMapFish before we can install it:"
+read -p "What version do you want to install? [2.5] " -r gmfver
+gmfver=${gmfver:-2.5}
+read -p "What is the fantastic name of your project? [my-super-gmf-app] " -r projname
+projname=${projname:-my-super-gmf-app}
+read -p "What coordinate system do you want to use? [2056] " -r srid
+srid=${srid:-2056}
+read -p "What extent do you want to use? [2420000,1030000,2900000,1350000] " -r extent
+extent=${extent:-2420000,1030000,2900000,1350000}
+
+# Git configuration
+while [ -z $gitmail ]
+do
+  read -p "What email do you want to use for git? " -r gitmail
+done
+while [ -z "$gitname" ]
+do
+  read -p "What name do you want to use for git? " -r gitname
+done
+
+echo "${Green}Version to install: $gmfver"
+echo "${Green}Project name      : $projname"
+echo "${Green}Coordinate system : $srid"
+echo "${Green}Extent            : $extent"
+echo "${Green}Project Directory : $projname"
+echo "${Green}Git Email         : $gitmail"
+echo "${Green}Git Name          : $gitname"
+
+echo "${Default}Please verify the configuration."
+read -p "${Default}Do you want to start the installation? [y/n] " -n 1 -r cont
+echo
+if ! [[ $cont =~ ^[Yy]$ ]]
+then
+  abort 94
+fi
+
+# Start installation
+####################
+
+echo
+echo "${Default}---------------------------------------------------------------------------"
+echo "${Default}Downloading containers..."
+docker pull camptocamp/geomapfish-tools:$gmfver
+docker pull camptocamp/geomapfish:$gmfver
+echo "${Green}OK."
+
+echo
+echo "${Default}---------------------------------------------------------------------------"
+
+# Create project
+echo "${Default}Creating GeoMapFish project..."
+docker run --rm -ti --volume=$(pwd):/src --env=SRID=$srid --env=EXTENT="$extent" camptocamp/geomapfish-tools:$gmfver run $(id -u) $(id -g) /src pcreate -s c2cgeoportal_create $projname > install.log
+echo "${Green}OK."
+
+# Update project
+echo "${Default}Updating project..."
+docker run --rm -ti --volume=$(pwd):/src --env=SRID=$srid --env=EXTENT="$extent" camptocamp/geomapfish-tools:$gmfver run $(id -u) $(id -g) /src pcreate -s c2cgeoportal_update $projname >> install.log
+echo "${Green}OK."
+
+# Correct error in .eslintrc file
+echo "${Default}Gathering positiveness..."
+cd $projname
+sed -i 's/code: 110/code: 200/g' geoportal/.eslintrc
+echo "${Green}PERFECT!"
+
+# Database configuration
+########################
+
+echo
+echo "${Default}---------------------------------------------------------------------------"
+echo "The first step is done. Now, we'll have to configure the database."
+echo "If you want, a test database can be installed locally automatically."
+echo "But if you already have configured one, it can be used."
+
+read -p "Do you want to download and configure a database automatically? [y/n] " -n 1 -r cont
+echo
+if [[ $cont =~ ^[Yy]$ ]]
+then
+  echo "Configuring GeoMapFish Database..."
+  
+  dbhost=172.17.0.1
+  # Check if the default network was overriden on this server
+  if [ -f /etc/docker/daemon.json ]
+  then
+    dbhost=`sed -r 's/.*"bip" *\: *"([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*/\1/' /etc/docker/daemon.json`
+  fi
+
+  dbport=5432
+  dbname=mydb
+  dbuser=www
+  dbpass=secret
+  
+  docker pull postgis/postgis:11-3.1
+  docker kill gmf_postgis >> ../install.log 2>&1
+  docker run --rm --name gmf_postgis -p 5432:5432 -v postgres:/var/lib/postgresql/data -e POSTGRES_PASSWORD=secret -d postgis/postgis:11-3.1 >> ../install.log
+  # Wait the postgres startup
+  sleep 20
+  docker exec gmf_postgis bash -c "psql -U postgres -c 'CREATE DATABASE mydb;'" >> ../install.log
+  docker exec gmf_postgis bash -c "psql -U postgres -d mydb -c 'CREATE EXTENSION postgis;'" >> ../install.log
+  docker exec gmf_postgis bash -c "psql -U postgres -d mydb -c 'CREATE EXTENSION hstore;'" >> ../install.log
+  docker exec gmf_postgis bash -c "psql -U postgres -d mydb -c \"CREATE USER www PASSWORD 'secret';\"" >> ../install.log
+  docker exec gmf_postgis bash -c "psql -U postgres -d mydb -c 'CREATE SCHEMA main;'" >> ../install.log
+  docker exec gmf_postgis bash -c "psql -U postgres -d mydb -c 'CREATE SCHEMA main_static;'" >> ../install.log
+  docker exec gmf_postgis bash -c "psql -U postgres -d mydb -c 'GRANT ALL ON SCHEMA main TO www;'" >> ../install.log
+  docker exec gmf_postgis bash -c "psql -U postgres -d mydb -c 'GRANT ALL ON SCHEMA main_static TO www;'" >> ../install.log
+  echo "${Green}OK." 
+else
+  echo "${Default}Ok, let's configure your database connection then.."
+  while [ -z $dbhost ]
+  do
+    read -p "Database Host: " -r dbhost
+  done
+  while [ -z $dbport ]
+  do
+    read -p "Database Port: " -r dbport
+  done
+  while [ -z $dbname ]
+  do
+    read -p "Database Name: " -r dbname
+  done
+  while [ -z $dbuser ]
+  do
+    read -p "Database User: " -r dbuser
+  done
+  while [ -z $dbpass ]
+  do
+    read -p "Database Password: " -r dbpass
+  done
+  
+  echo "${Default}Please verify the configuration."
+  echo "${Green}Database Host    : $dbhost"
+  echo "${Green}Database Port    : $dbport"
+  echo "${Green}Database Name    : $dbname"
+  echo "${Green}Database User    : $dbuser"
+  echo "${Green}Database Password: $dbpass"
+
+  read -p "${Default}Do you want to continue? [y/n] " -n 1 -r cont
+  echo
+  if ! [[ $cont =~ ^[Yy]$ ]]
+  then
+    abort 95
+  fi
+
+fi
+
+# Env configuration
+###################
+
+echo
+echo "${Default}---------------------------------------------------------------------------"
+echo "Configuring GeoMapFish project..."
+sed -i "s/PGDATABASE=gmf_.*/PGDATABASE=${dbname}/g" env.project
+sed -i "s/PGHOST=pg-gs.camptocamp.com/PGHOST=${dbhost}/g" env.project
+sed -i "s/PGHOST_SLAVE=pg-gs.camptocamp.com/PGHOST_SLAVE=${dbhost}/g" env.project
+sed -i "s/PGPORT=30100/PGPORT=${dbport}/g" env.project
+sed -i "s/PGPORT_SLAVE=30101/PGPORT_SLAVE=${dbport}/g" env.project
+sed -i "s/PGUSER=<user>/PGUSER=${dbuser}/g" env.project
+sed -i "s/PGPASSWORD=<pass>/PGPASSWORD=${dbpass}/g" env.project
+sed -i "s/PGSSLMODE=require/PGSSLMODE=prefer/g" env.project
+sed -i "s/VISIBLE_WEB_HOST=localhost/VISIBLE_WEB_HOST=${gmf_host}/g" env.default
+sed -i "s/8484/${gmf_port}/g" env.default
+echo "${Green}OK."
+
+# Initialize git and firt commit
+echo "${Default}Committing first version..."
+git init . >> ../install.log
+git add . >> ../install.log
+git config user.email "$gitmail" >> ../install.log
+git config user.name "$gitname" >> ../install.log
+git commit -m "First commit" >> ../install.log
+echo "${Green}OK."
+
+# Build the app
+###############
+echo "${Default}Compiling GeoMapFish project..."
+./build >> ../install.log
+echo "${Green}OK."
+
+# Start the app
+###############
+echo "${Default}Starting GeoMapFish..."
+docker-compose up -d
+echo "${Green}OK."
+
+# Fix proxy error
+#################
+fix_proxy()
+{
+  if [[ "$2" = 'top' ]]
+  then
+    echo "def _fix_case(env):
+    if 'http_proxy' in env and 'HTTP_PROXY' in env:
+        env.pop('http_proxy')
+    if 'https_proxy' in env and 'HTTPS_PROXY' in env:
+        env.pop('https_proxy')
+    if 'no_proxy' in env and 'NO_PROXY' in env:
+        env.pop('no_proxy')
+    return env
+
+" | cat - $1 > temp && mv temp $1
+  else
+    echo "
+
+def _fix_case(env):
+    if 'http_proxy' in env and 'HTTP_PROXY' in env:
+        env.pop('http_proxy')
+    if 'https_proxy' in env and 'HTTPS_PROXY' in env:
+        env.pop('https_proxy')
+    if 'no_proxy' in env and 'NO_PROXY' in env:
+        env.pop('no_proxy')
+    return env" >> $1
+  fi
+
+  sed -i "s/dict(os.environ)/_fix_case(dict(os.environ))/g" $1
+}
+
+if [ "$using_proxy" = true ]
+then
+  echo
+  echo "${Default}---------------------------------------------------------------------------"
+  echo "${Blue}There's a problem with proxies... This has to be clarified with C2C"
+  echo "${Blue}For the moment, we'll try to fix the problem with a custom solution"
+  containerprefix=`echo 'my-super-gmf-app' | sed -r 's/-/_/g'`
+  mkdir __fix
+  docker cp ${containerprefix}_geoportal_1:/opt/c2cwsgiutils/c2cwsgiutils/pyramid_logging.py __fix/pyramid_logging.py
+  docker cp ${containerprefix}_geoportal_1:/opt/alembic/env.py __fix/env.py
+  docker cp ${containerprefix}_tilegeneration_slave_1:/app/tilecloud_chain/__init__.py __fix/__init__.py
+  fix_proxy "__fix/pyramid_logging.py"
+  fix_proxy "__fix/env.py" "top"
+  fix_proxy "__fix/__init__.py"
+  wget --quiet "https://raw.githubusercontent.com/geomapfish/getting_started/main/docker-compose.yaml" -O docker-compose.yaml
+  echo "${Green}OK."
+
+  echo "${Default}Restarting GeoMapFish..."
+  docker-compose down && docker-compose up -d
+fi
+
+# Create schemas
+################
+echo "${Default}Initializing Database..."
+docker-compose exec geoportal alembic --name=main upgrade head
+docker-compose exec geoportal alembic --name=static upgrade head
+echo "${Green}OK."
+
+echo
+echo "${Default}---------------------------------------------------------------------------"
+echo "${Green}DONE!"
+echo "${Default}The application can be accessed at https://$gmf_host:$gmf_port"
+echo "The next things to do:"
+echo "- Connect to the application with admin/admin and change the password."
+echo "- Go at https://$gmf_host:$gmf_port/admin and add your own data."
+echo "- Enjoy !"
